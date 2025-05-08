dda9ab7eb09a7725e48415c185cef94380a6de31
cbisac <cortesbuitragoisac@gmail.com>
2025-05-08 03:27:29 -0500

    fix: merge issues with user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,23 +1,18 @@
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { Op } from "sequelize";
-import { User } from "../models/user.model.js";
-import nodemailer from "nodemailer";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { Op } from 'sequelize'
+import { User } from '../models/user.model.js'
+import nodemailer from 'nodemailer'
 
 export class UserController {
   getAllUsers = async (req, res) => {
     try {
-<<<<<<< HEAD
-      const users = await User.findAll();
-      res.json(users);
-=======
       const users = await User.findAll({ attributes: { exclude: ['password'] } })
       res.json(users)
->>>>>>> 898f0609d5ca51239647e9cd34ab6d88b876c7c3
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: error.message })
     }
-  };
+  }
 
   createUser = async (req, res) => {
     try {
@@ -30,27 +25,27 @@ export class UserController {
         secondLastName,
         major,
         role,
-        age,
-      } = req.body;
+        age
+      } = req.body
 
-      if (!email.endsWith("@elpoli.edu.co")) {
+      if (!email.endsWith('@elpoli.edu.co')) {
         return res
           .status(400)
-          .json({ message: "Email must end with @elpoli.edu.co" });
+          .json({ message: 'Email must end with @elpoli.edu.co' })
       }
 
-      if (!["student", "monitor"].includes(role)) {
+      if (!['student', 'monitor'].includes(role)) {
         return res
           .status(400)
-          .json({ message: "Invalid role. Must be Student or Monitor" });
+          .json({ message: 'Invalid role. Must be Student or Monitor' })
       }
 
-      const existingUser = await User.findByPk(email);
+      const existingUser = await User.findByPk(email)
       if (existingUser) {
-        return res.status(400).json({ message: "User already exists" });
+        return res.status(400).json({ message: 'User already exists' })
       }
 
-      const cryptedPassword = await bcrypt.hash(password, 12);
+      const cryptedPassword = await bcrypt.hash(password, 12)
 
       const newUser = await User.create({
         email,
@@ -61,124 +56,119 @@ export class UserController {
         secondLastName,
         major,
         role,
-        age,
-      });
+        age
+      })
 
-      res.status(201).json(newUser);
+      res.status(201).json(newUser)
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: error.message })
     }
-  };
+  }
 
   getUser = async (req, res) => {
     try {
-<<<<<<< HEAD
-      const { email } = req.params;
-      const user = await User.findByPk(email);
-=======
       const { email } = req.params
       const user = await User.findByPk(email, { attributes: { exclude: ['password'] } })
->>>>>>> 898f0609d5ca51239647e9cd34ab6d88b876c7c3
       if (user) {
-        res.json(user);
+        res.json(user)
       } else {
-        res.status(404).json({ message: "User not found" });
+        res.status(404).json({ message: 'User not found' })
       }
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: error.message })
     }
-  };
+  }
 
   updateUser = async (req, res) => {
     try {
-      const { email } = req.params;
-      const user = await User.findByPk(email);
+      const { email } = req.params
+      const user = await User.findByPk(email)
       if (!user) {
-        return res.status(404).json({ message: "User not found" });
+        return res.status(404).json({ message: 'User not found' })
       }
 
       if (req.body.password) {
-        req.body.password = await bcrypt.hash(req.body.password, 12);
+        req.body.password = await bcrypt.hash(req.body.password, 12)
       }
 
-      await user.update(req.body);
-      res.status(202).json(user);
+      await user.update(req.body)
+      res.status(202).json(user)
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: error.message })
     }
-  };
+  }
 
   deleteUser = async (req, res) => {
     try {
-      const { email } = req.params;
-      await User.destroy({ where: { email } });
-      res.json({ message: "User deleted" });
+      const { email } = req.params
+      await User.destroy({ where: { email } })
+      res.json({ message: 'User deleted' })
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: error.message })
     }
-  };
+  }
 
   login = async (req, res) => {
     try {
-      const { email, password } = req.body;
+      const { email, password } = req.body
       const user = await User.findOne({
         where: {
           email: {
-            [Op.iLike]: email,
-          },
-        },
-      });
+            [Op.iLike]: email
+          }
+        }
+      })
 
       if (!user) {
-        return res.status(400).json({ err: "Email is not registered" });
+        return res.status(400).json({ err: 'Email is not registered' })
       }
 
-      const passwordMatch = await bcrypt.compare(password, user.password);
+      const passwordMatch = await bcrypt.compare(password, user.password)
       if (!passwordMatch) {
-        return res.status(401).json({ err: "Password incorrect" });
+        return res.status(401).json({ err: 'Password incorrect' })
       }
       const token = jwt.sign(
         {
           email: user.email,
           role: user.role,
-          id: user.email,
+          id: user.email
         },
-        process.env.SECRET_KEY ?? "LOLOMANSOLO",
-        { expiresIn: "2h" }
-      );
+        process.env.SECRET_KEY ?? 'LOLOMANSOLO',
+        { expiresIn: '2h' }
+      )
 
       res.json({
         token,
         role: user.role,
         email: user.email,
-        firstName: user.firstName,
-      });
+        firstName: user.firstName
+      })
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: error.message })
     }
-  };
+  }
 
   requestPasswordreset = async (req, res) => {
     try {
-      const { email } = req.body;
-      const user = await User.findByPk(email);
+      const { email } = req.body
+      const user = await User.findByPk(email)
       if (!user) {
-        return res.status(404).json({ message: "User not found" });
+        return res.status(404).json({ message: 'User not found' })
       }
 
       const token = jwt.sign(
         { email },
-        process.env.RESET_KEY ?? "LOLOMANSOLO2",
+        process.env.RESET_KEY ?? 'LOLOMANSOLO2',
         {
-          expiresIn: "1h",
+          expiresIn: '1h'
         }
-      );
+      )
 
-      const resetLink = `http://localhost:4200/reset-password?token=${token}`;
+      const resetLink = `http://localhost:4200/reset-password?token=${token}`
 
       await sendEmail(
         email,
-        "Password Reset Request",
+        'Password Reset Request',
         `<body style="margin: 0; padding: 0; background-color: #194866; font-family: Arial, sans-serif; color: white;">
           <div style="max-width: 500px; margin: 40px auto; background-color: #253046; padding: 40px; border-radius: 10px; text-align: center;">
             <h1 style="color: #f3a32f; font-size: 36px; margin-bottom: 20px;">StayOn</h1>
@@ -191,49 +181,48 @@ export class UserController {
             </a>
           </div>
         </body>`
-      );
+      )
 
-      res.json({ message: "Password reset link sent to youar email" });
+      res.json({ message: 'Password reset link sent to youar email' })
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: error.message })
     }
-  };
+  }
 
   changePassword = async (req, res) => {
     try {
-      const { token, newPassword } = req.body;
+      const { token, newPassword } = req.body
       const decoded = jwt.verify(
         token,
-        process.env.RESET_KEY ?? "LOLOMANSOLO2"
-      );
-      const user = await User.findByPk(decoded.email);
+        process.env.RESET_KEY ?? 'LOLOMANSOLO2'
+      )
+      const user = await User.findByPk(decoded.email)
 
-      if (!user)
-        return res.status(400).json({ messaage: "invalid or expired token" });
+      if (!user) { return res.status(400).json({ messaage: 'invalid or expired token' }) }
 
-      user.password = await bcrypt.hash(newPassword, 10);
-      await user.save();
+      user.password = await bcrypt.hash(newPassword, 10)
+      await user.save()
 
-      res.status(200).json({ message: "Password changed successfully" });
+      res.status(200).json({ message: 'Password changed successfully' })
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      res.status(500).json({ message: error.message })
     }
-  };
+  }
 }
 
-async function sendEmail(to, subject, html) {
+async function sendEmail (to, subject, html) {
   const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    service: 'Gmail',
     auth: {
-      user: "juanjo.jjev@gmail.com",
-      pass: "rhxs cmcb nbzg onuf",
-    },
-  });
+      user: 'juanjo.jjev@gmail.com',
+      pass: 'rhxs cmcb nbzg onuf'
+    }
+  })
 
   await transporter.sendMail({
     from: '"StayOn" <no-reply@StayOn.com>',
     to,
     subject,
-    html,
-  });
+    html
+  })
 }
