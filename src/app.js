6ee1b2f34e506b8e2d2dbd4ed9f0698bf85c90d6
Embm5 <emmanuel_bolivar82212@elpoli.edu.co>
2025-06-02 12:27:16 -0500

    Add MVC Meeting
    added MVC of meetting

--- src/app.js
+++ src/app.js
@@ -1,18 +1,20 @@
-import express from 'express'
-import 'dotenv/config'
-import cors from 'cors'
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
 
-import { userRouters } from './routes/user.routes.js'
-import { postRouters } from './routes/post.routes.js'
-import { profileRouters } from './routes/profile.routes.js'
+import { userRouters } from "./routes/user.routes.js";
+import { postRouters } from "./routes/post.routes.js";
+import { profileRouters } from "./routes/profile.routes.js";
+import { meetingRouters } from "./routes/meeting.routes.js";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use(userRouters)
-app.use(postRouters)
-app.use(profileRouters)
+app.use(userRouters);
+app.use(postRouters);
+app.use(profileRouters);
+app.use(meetingRouters);
 
-export default app
+export default app;
--- src/routes/meeting.routes.js
+++ src/routes/meeting.routes.js
@@ -1,11 +1,19 @@
-import { Router } from "express";
 import { MeetingController } from "../controllers/meeting.controller.js";
+import { Router } from "express";
 
-const meetingController = new MeetingController();
 export const meetingRouters = Router();
 
-meetingRouters.get("/api/meetings", meetingController.getAllMeetings);
-meetingRouters.get("/api/meetings/:id", meetingController.getMeeting);
-meetingRouters.post("/api/meetings", meetingController.createMeeting);
-meetingRouters.put("/api/meetings/:id", meetingController.updateMeeting);
-meetingRouters.delete("/api/meetings/:id", meetingController.deleteMeeting);
+meetingRouters.get("/api/meetings", MeetingController.getAllMeetings);
+meetingRouters.get("/api/meetings/:id", MeetingController.getMeeting);
+meetingRouters.get(
+  "/api/meetings/user/:userId",
+  MeetingController.getMeetingsByUser
+);
+meetingRouters.post("/api/meetings", MeetingController.createMeeting);
+meetingRouters.put("/api/meetings/:id", MeetingController.updateMeeting);
+meetingRouters.delete("/api/meetings/:id", MeetingController.deleteMeeting);
+meetingRouters.get("/api/meetings/active", MeetingController.getActiveMeetings);
+meetingRouters.patch(
+  "/api/meetings/:id/deactivate",
+  MeetingController.deactivateMeeting
+);
